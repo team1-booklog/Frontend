8e216db45a527e97c5d528e5847ed82a439941b3
Yunseok <watemark1017@gmail.com>
2024-10-07 00:43:49 +0900

    [FEAT] - 내 책 목록 페이지 만들기

--- src/components/MyPage/BookList.tsx
+++ src/components/MyPage/BookList.tsx
@@ -1,13 +1,46 @@
+import BookCardComponent from "../common/BookCardComponent";
+
 export default function BookList() {
+  // 레이아웃을 위한 임시 데이터
+  const tempData = {
+    bookList: [
+      {
+        title: 'Doit! 점프 투 파이썬',
+        imageUrl: 'https://image9.coupangcdn.com/image/vendor_inventory/9ade/a02d54da37050318e16a716ef452616fa0d747061b44b77602408184a384.jpg'
+      },
+      {
+        title: '실전 스벨트 & 스벨트킷 입문',
+        imageUrl: 'https://image.yes24.com/YES24ViewerDatas/Z1260_LT/A12594/B125932/125931860_L/15zjwcxrq2qdmcdx01.jpg'
+      },
+      {
+        title: '누가 내 머리에 똥쌌어?',
+        imageUrl: 'https://image.yes24.com/YES24ViewerDatas/Z1_LT/A1/B2/1315_L/z35llww4yi7bw722%EB%88%84%EA%B0%80.jpg'
+      },
+      {
+        title: '구름빵',
+        imageUrl: 'https://image.yes24.com/goods/1472361/XL'
+      },
+      {
+        title: '운영체제',
+        imageUrl: 'https://image.yes24.com/goods/89496122/XL'
+      },
+      {
+        title: '알고리즘 문제 해결 전략',
+        imageUrl: 'https://image.yes24.com/goods/8006522/XL'
+      }
+    ]
+  }
+
   return (
-    <div className="px-60 py-20">
-      <h2 className="text-2xl font-semibold mb-3">최근 읽은 책</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="bg-white p-4 rounded-lg shadow-md">
-          <img src="https://via.placeholder.com/150" alt="book" className="w-full mb-4"/>
-          <h3 className="text-lg font-semibold">책 제목</h3>
-          <p className="text-sm">작가명</p>
-        </div>
+    <div className="flex justify-center">
+      <div className="grid grid-cols-3 xl:grid-cols-4 gap-4 max-w-5xl mx-auto">
+        {tempData.bookList.map((book, index) => (
+          <BookCardComponent 
+            key={index} 
+            title={book.title} 
+            imageUrl={book.imageUrl}
+          />
+        ))}
       </div>
     </div>
   );
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -5,21 +5,21 @@ export default function MyPage() {
   
   // 임시로 하드코딩한 유저 데이터
   const userName = '홍길동';
-  const bookCount = 0;
+  const bookCount = 4;
   const bookReportCount = 15;
 
   return (
     <>
-      <div className="px-10 md:px-60 pt-14 md:pt-28">
-        <h1 className="text-xl md:text-4xl font-semibold mb-3">
+      <div className="px-10 md:px-32 pt-14 md:pt-28">
+        <h1 className="text-xl md:text-3xl lg:text-4xl font-semibold mb-3">
           {userName}
         </h1>
-        <p className="text-base md:text-2xl mb-6 md:mb-12">
+        <p className="text-base md:text-xl lg:text-2xl">
           지금까지 <span className="text-[#EC6B53]">{bookCount}권</span>의 책을 읽고 <span className="text-[#EC6B53]">{bookReportCount}개</span>의 독후감을 작성했어요.
         </p>
-        <hr />
       </div>
-      <div>
+      <div className="px-10 md:px-16 xl:px-32 py-20">
+        <hr className="mb-6 md:mb-12"/>
       { (bookCount === 0) ? <NoBook /> : <BookList /> }
       </div>
     </>
