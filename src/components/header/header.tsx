674d52d062203c8340f9b7813fe810072220f4bd
Yunseok <watemark1017@gmail.com>
2024-10-06 21:10:21 +0900

    [FEAT] - 헤더 클릭시 페이지 이동 추가 + Login.tsx 파일 만들기

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import { PiSignIn, PiSignOut } from "react-icons/pi";
 import cn from '../../libs/cn.ts';
+
 export default function Header() {
+  const navigate = useNavigate();
+  const handleLogoClick = () => {
+    navigate("/");
+  }
+  const handleUserNameClick = () => {
+    navigate("/mypage");
+  }
+  const handleLoginClick = () => {
+    navigate("/login");
+  }
+
   // 임시로 땜방한 로그인 여부, 유저네임
   const isLogin = true;
   const userName = '홍길동';
@@ -8,10 +21,13 @@ export default function Header() {
   return (
     <header className="bg-[#2B5877] text-white p-4">
       <div className="flex justify-between mx-0 md:mx-20">
-      <h1 className="text-3xl ">Booklog</h1>
+      <h1 onClick={handleLogoClick} className="text-3xl ">Booklog</h1>
       {isLogin ?
         <div>
-          <span className="mr-14 text-sm md:text-xl">
+          <span 
+            onClick={handleUserNameClick}
+            className="mr-14 text-sm md:text-xl"
+          >
             {userName} 님
           </span>
           <button className={cn(
@@ -22,7 +38,10 @@ export default function Header() {
           </button> 
         </div>     
       :  
-        <button className="bg-white px-4 py-2 rounded-lg text-[#EC6B53] font-bold">
+        <button 
+          onClick={handleLoginClick}
+          className="bg-white px-4 py-2 rounded-lg text-[#EC6B53] font-bold"
+        >
           <PiSignIn className="text-base md:text-2xl inline"/>
           로그인
         </button>    
