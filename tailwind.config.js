c40a76f54f0ce0bcf77bf7ec1763f62abf1ca566
gmlwlsdl <heejin094@naver.com>
2024-10-07 10:45:08 +0900

    [STYLE] - Rockwell 폰트 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rockwell: ['Rockwell'],
+      },
+      fontWeight: {
+        normal: 400,
+        bold: 700,
+      },
+    },
   },
   plugins: [],
 }
