d4a7a74cb98aac3a399d6a42071ad35770450b5e
Yunseok <watemark1017@gmail.com>
2024-10-07 20:55:58 +0900

    [STYLE] - 포커스시 placeholder 안보이게 하기

--- src/components/MainPage/SearchBar.tsx
+++ src/components/MainPage/SearchBar.tsx
@@ -8,6 +8,7 @@ interface SearchBarProps {
 
 export default function SearchBar({ onSearch }: SearchBarProps) {
   const [searchText, setSearchText] = useState('');
+  const [placeholder, setPlaceholder] = useState('궁금한 책 제목이나 저자를 검색해보세요!');
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   const handleButtonClick = () => {
@@ -34,29 +35,35 @@ export default function SearchBar({ onSearch }: SearchBarProps) {
 
   return (
     <div
-  className={cn(
-    'relative w-full flex justify-center items-center mt-6 md:mt-20 gap-2',
-    'font-rockwell font-normal px-4 md:px-10',
-  )}
->
-  <div className="relative w-full max-w-[1200px] border-b-2 border-[#2B5877]">
-    <input
-      type="text"
-      placeholder="궁금한 책 제목이나 저자를 검색해보세요!"
-      value={searchText}
-      ref={inputRef}
-      onKeyDown={handleKeyDown}
-      onChange={(e) => setSearchText(e.target.value)}
       className={cn(
-        'w-full p-2 text-center', // 반응형 너비
-        'text-base md:text-xl xl:text-3xl focus:outline-none'
+        'relative w-full flex justify-center items-center mt-6 md:mt-20 gap-2',
+        'font-rockwell font-normal px-4 md:px-10',
       )}
-    />
-    <button onClick={handleButtonClick} className="absolute right-0 mr-2">
-      <img src={MagnifyingGlass} alt="Search" className="w-4 md:w-6 h-4 md:h-6" />
-    </button>
-  </div>
-</div>
+    >
+      <div className="relative w-full max-w-[1200px] border-b-2 border-[#2B5877]">
+        <input
+          type="text"
+          placeholder={placeholder}
+          onFocus={() => setPlaceholder('')}
+          onBlur={() => setPlaceholder('궁금한 책 제목이나 저자를 검색해보세요!')}
+          value={searchText}
+          ref={inputRef}
+          onKeyDown={handleKeyDown}
+          onChange={(e) => setSearchText(e.target.value)}
+          className={cn(
+            'w-full p-2 text-center', // 반응형 너비
+            'text-base md:text-xl xl:text-3xl focus:outline-none'
+          )}
+        />
+        <button onClick={handleButtonClick} className="absolute right-0 mr-2">
+          <img 
+            src={MagnifyingGlass} 
+            alt="Search" 
+            className="w-4 md:w-6 h-4 md:h-6" 
+          />
+        </button>
+      </div>
+    </div>
 
   );
 }
