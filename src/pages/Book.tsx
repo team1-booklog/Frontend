64f8132c47ee36f37a5a04b23393a7b359ae3aca
gmlwlsdl <heejin094@naver.com>
2024-10-08 14:46:59 +0900

    [FEAT] - 책 검색 API 연동
    - CurrentlyTrendingBook에 도서의 isbn 하드코딩

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -1,12 +1,32 @@
-import { useParams } from 'react-router-dom'
+import { useBook } from '../hooks/UseBook'
 
 export default function Book() {
-  const { bookSlug } = useParams()
+  const { bookData, error } = useBook()
+
+  if (error) {
+    return <p>Error: {error}</p>
+  }
 
   return (
     <div>
       <h1>Book Detail Page</h1>
-      <p>Book Slug: {bookSlug}</p>
+      {bookData ? (
+        <div>
+          <p>Title: {bookData.title}</p>
+          <p>Author: {bookData.author}</p>
+          <p>Publisher: {bookData.publisher}</p>
+          <p>Publication Date: {bookData.pubdate}</p>
+          <p>ISBN: {bookData.isbn}</p>
+          <p>Price: {bookData.discount} 원</p>
+          <p>Description: {bookData.description}</p>
+          <a href={bookData.link} target="_blank" rel="noopener noreferrer">
+            자세히 보기
+          </a>
+          <img src={bookData.image} alt={bookData.title} />
+        </div>
+      ) : (
+        <p>No book data available.</p>
+      )}
     </div>
   )
 }
