b70bf7b654641868d8e7360841823891e8d44e0f
Yunseok <watemark1017@gmail.com>
2024-10-09 16:56:35 +0900

    [REFACTOR] - 메인페이지 작업 전 컴포넌트화로 가독성 높여보기

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import cn from '../libs/cn.ts';
+import MainPageContext from '../components/MainPage/MainPageContext.tsx';
 import SearchBar from '../components/MainPage/SearchBar.tsx';
 import CurrentlyTrendingBook from '../components/MainPage/CurrentlyTrendingBook.tsx';
 
@@ -13,29 +13,7 @@ export default function MainPage() {
 
   return (
     <>
-      <div
-        className={cn(
-          'relative w-full flex-col justify-center text-center',
-          'flex flex-col items-center gap-3 md:gap-6',
-          'w-fit font-rockwell font-normal'
-        )}
-      >
-        <h1 
-          className={cn(
-            "w-fit text-4xl md:text-6xl xl:text-8xl text-[#2B5877]",
-            "mt-14 md:mt-20 xl:mt-32"
-          )}
-          style={{ letterSpacing: '0.1em' }}
-        >
-          BookLog
-        </h1>
-        <h2 className={cn(
-          "w-fit text-base md:text-2xl xl:text-4xl text-[#918F8F]",
-          'mb-6 md:mb-0'
-        )}>
-          join our community and share your book reviews.
-        </h2>
-      </div>
+      <MainPageContext />
 
       {/* Search Bar */}
       <SearchBar onSearch={handleSearch} />
