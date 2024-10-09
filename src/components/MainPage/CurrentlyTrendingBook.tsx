e6e97a060bf5d128962d25f46ecea97099f33f75
Yunseok <watemark1017@gmail.com>
2024-10-09 17:44:05 +0900

    [FEAT] - 검색 결과 카드 컴포넌트 형식으로 보이게 수정

--- src/components/MainPage/CurrentlyTrendingBook.tsx
+++ src/components/MainPage/CurrentlyTrendingBook.tsx
@@ -9,32 +9,32 @@ export default function CurrentlyTrendingBook() {
   const tempData = {
     bookList: [
       {
-        isbn: 9791163034735,
+        isbn: '9791163034735',
         title: 'Doit! 점프 투 파이썬',
         imageUrl:
           'https://image9.coupangcdn.com/image/vendor_inventory/9ade/a02d54da37050318e16a716ef452616fa0d747061b44b77602408184a384.jpg',
       },
       {
-        isbn: 9791193926161,
+        isbn: '9791193926161',
         title: '실전 스벨트 & 스벨트킷 입문',
         imageUrl:
           'https://image.yes24.com/YES24ViewerDatas/Z1260_LT/A12594/B125932/125931860_L/15zjwcxrq2qdmcdx01.jpg',
       },
       {
-        isbn: 9788971968413,
+        isbn: '9788971968413',
         title: '누가 내 머리에 똥쌌어?',
         imageUrl:
           'https://image.yes24.com/YES24ViewerDatas/Z1_LT/A1/B2/1315_L/z35llww4yi7bw722%EB%88%84%EA%B0%80.jpg',
       },
       {
-        isbn: 9791170283836,
+        isbn: '9791170283836',
         title: '구름빵',
         imageUrl: 'https://image.yes24.com/goods/1472361/XL',
       },
     ],
   }
 
-  const handleBookClick = (isbn: number) => {
+  const handleBookClick = (isbn: string) => {
     const bookSlug = isbn.toString()
     navigate(`/book/${bookSlug}`)
   }
--- src/components/MainPage/SearchResult.tsx
+++ src/components/MainPage/SearchResult.tsx
@@ -1,33 +1,47 @@
-import { useSearchBookByName } from '../../hooks/UseSearchBookbyName'
-import { BookData } from '../../model/BookData'
+import { useSearchBookByName } from '../../hooks/UseSearchBookbyName';
+import { BookData } from '../../model/BookData';
+import BookCardComponent from '../common/BookCardComponent';
+import cn from '../../libs/cn.ts';
 
 interface SearchResultProps {
-  searchTerm: string
+  searchTerm: string;
 }
 
 export default function SearchResult({ searchTerm }: SearchResultProps) {
-  const { books, error, loading } = useSearchBookByName(searchTerm)
+  const { books, error, loading } = useSearchBookByName(searchTerm);
+
+  const handleBookClick = (isbn: string) => {
+    const bookSlug = isbn.toString();
+    window.open(`/book/${bookSlug}`, '_blank'); // 새 탭에서 열기
+  };
 
   return (
-    <div>
-      <h1>Search Result Page</h1>
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {!loading && !error && books.length === 0 && <p>No results found.</p>}
-      <ul>
-        {books.map((book: BookData, index: number) => (
-          <li key={index}>
-            <h2>{book.title}</h2>
-            <p>{book.author}</p>
-            <img src={book.image} alt={book.title} />
-            <p>{book.publisher}</p>
-            <p>{book.pubdate}</p>
-            <a href={book.link} target="_blank" rel="noopener noreferrer">
-              More Info
-            </a>
-          </li>
-        ))}
-      </ul>
+    <div className="p-4">
+      <h1
+        className={cn(
+          'text-2xl md:text-3xl lg:text-4xl font-semibold text-center',
+          'mb-6 text-[#EC6B53]'
+        )}
+      >
+        검색 결과
+      </h1>
+      {loading && <p className="text-center">Loading...</p>}
+      {error && <p className="text-center text-red-500">{error}</p>}
+      {!loading && !error && books.length === 0 && (
+        <p className="text-center">검색 결과가 없습니다.</p>
+      )}
+      <div className="flex justify-center">
+        <div className="grid grid-cols-3 xl:grid-cols-4 gap-4 max-w-5xl mx-auto px-4">
+          {!loading && !error && books.map((book: BookData, index: number) => (
+            <BookCardComponent
+              key={index}
+              title={book.title}
+              imageUrl={book.image}
+              onClick={() => handleBookClick(book.isbn)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
