37ff9df8646c32c12d91821cdfb791f51ba4b354
Yunseok <watemark1017@gmail.com>
2024-10-11 00:24:53 +0900

    Revert "[STYLE] -  공통 책카드 컴포넌트가 잘 보이게 수정"
    This reverts commit 6c985b3673576bf0feae460f451fcee7b3ed90e0.

--- src/components/common/BookCardComponent.tsx
+++ src/components/common/BookCardComponent.tsx
@@ -20,9 +20,7 @@ export default function BookCardComponent({
         src={imageUrl}
         alt="book"
         className={cn(
-          // w-32 md:w-48 lg:w-60 h-40 md:h-60 lg:h-80
-          'aspect-[3/4] relative w-full h-full',
-          'object-cover mb-4 rounded-lg shadow-lg',
+          'w-32 h-40 md:w-48 md:h-60 lg:w-60 lg:h-80 object-cover mb-4 rounded-lg shadow-lg',
           'transform transition duration-300 ease-in-out',
           // 호버 시 크기, 그림자, 위치 변경
           'hover:scale-105 hover:shadow-2xl hover:translate-y-2'
