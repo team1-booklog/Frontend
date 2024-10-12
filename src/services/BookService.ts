921ee4cee4b3d0c287584c395a093fdd16005978
bluekmky1 <115731932+bluekmky1@users.noreply.github.com>
2024-10-12 16:20:51 +0900

    [FEAT] - 마이페이지의 내가 읽은 책 불러오는 Api 연동

--- src/services/BookService.ts
+++ src/services/BookService.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { NAVER_API_HEADERS, getBookSearchUrl } from '../config/BookClient'
 import { BookData } from '../model/BookData'
+import apiClient from '../config/ApiClient'
+import { UserActivity } from '../model/user/UserActivity'
 
 const ERROR_MESSAGES = {
   NOT_FOUND: '책 데이터를 찾을 수 없습니다.',
@@ -19,6 +21,37 @@ export const fetchBookData = async (
     const data = response.data
     if (data.items && data.items.length > 0) {
       return data.items[0]
+    } else {
+      throw new Error(ERROR_MESSAGES.NOT_FOUND)
+    }
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      throw new Error(
+        error.response.data.message || ERROR_MESSAGES.API_REQUEST_FAILED
+      )
+    } else {
+      throw new Error(ERROR_MESSAGES.UNKNOWN_ERROR)
+    }
+  }
+}
+
+export const getUseReadBookList = async (): Promise<UserActivity | null> => {
+  try {
+    // auth인터셉터가 제대로 작동하지 않아서 결과를 확인하기 위한 at 하드코딩
+
+    // const config = {
+    //   headers: {
+    //     Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1bW1kZXYwM0BnbWFpbC5jb20iLCJpYXQiOjE3Mjg3MTU4NzcsImV4cCI6MTcyODcyMzA3Nywic3ViIjoiYmx1ZWtta3kiLCJpZCI6ImJsdWVrbWt5In0.9v2Pc1TxAbMpL5rfamuxrBW3G27qSoAtBFPjGddVWlY`,
+    //   },
+    // }
+
+    // const response = await apiClient.get('books/me', config)
+
+    const response = await apiClient.get('books/me')
+
+    const data = response.data
+    if (data) {
+      return data
     } else {
       throw new Error(ERROR_MESSAGES.NOT_FOUND)
     }
