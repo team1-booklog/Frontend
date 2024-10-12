a986cc3fea59eb1942530126c9f68595890c50ff
Yunseok <watemark1017@gmail.com>
2024-10-12 17:15:43 +0900

    [FEAT] - ISBN으로 Bookid 얻기

---
+++ src/services/IsbnBookIdService.ts
@@ -0,0 +1,23 @@
+import apiClient from '../config/ApiClient';
+
+interface BookIdResponse {
+  id: number;
+  title: string;
+}
+
+// ISBN으로 책 ID 받아오기
+export const fetchBookId = async (isbn: string): Promise<number | null> => {
+  try {
+    const response = await apiClient.get<BookIdResponse>(`/books/isbn/${isbn}`);
+    const data = response.data;
+
+    if (data && data.id) {
+      return data.id;
+    } else {
+      throw new Error('책 데이터를 찾을 수 없습니다.');
+    }
+  } catch (error) {
+    console.error('에러가 발생했습니다. : ', error);
+    return null;
+  }
+};
